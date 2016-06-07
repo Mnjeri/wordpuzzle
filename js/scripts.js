6da294448dccddf88adde528b50e44cbae6119ad
Charles Ewel and Maldon Meehan <maldon@>
2016-06-07 08:54:43 -0700

    create a functional word puzzle

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("form#wordpuzzleform").submit(function(event) {
+    var stringinput=$("input#textinput").val();
+    var inputletters=stringinput.split("");
+    console.log(inputletters);
+    for(var index=0; index < inputletters.length; index+=1){
+        if(inputletters[index]==="a" || inputletters[index]==="e" || inputletters[index]==="i" || inputletters[index]==="o" || inputletters[index]==="u") {
+          inputletters[index]="-"
+        }
+    }
+    var puzzlestring = inputletters.join("");
+    console.log(puzzlestring)
+    $(".outputstring").text(puzzlestring)
+    event.preventDefault();
+  });
+});
